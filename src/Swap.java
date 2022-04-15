162a808a0f13c941bab58f71f9c6e16ff705519e
prashanth <reddy8129@gmail.com>
2022-04-15 22:36:10 +0530

    Swap.java updated

--- src/Swap.java
+++ src/Swap.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Swap {
 	public static void main(String[] args) {
+		System.out.println("Java Program to swap two numbers without using help variable");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("enter the first num");
 		int a=scan.nextInt();
